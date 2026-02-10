3167bd91899466e8c4cb6b314d80a681f6264d78
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2026-02-10 14:06:12 -0800

    Add map component on single property page

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,6 +1,11 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { setDefaults, fromAddress } from 'react-geocode';
+import Map, { Marker } from 'react-map-gl/mapbox';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import Image from 'next/image';
+import pin from '@/assets/images/pin.svg';
+import Spinner from './Spinner';
 
 const PropertyMap = ({ property }) => {
   const [lat, setLat] = useState(null);
@@ -53,12 +58,29 @@ const PropertyMap = ({ property }) => {
     fetchCoords();
   }, []);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <Spinner />;
 
   if (geocodeError)
     return <div className='text-xl'>No location data found</div>;
 
-  return <div>Map</div>;
+  return (
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: '100%', height: 500 }}
+        mapStyle='mapbox://styles/mapbox/streets-v9'
+      >
+        <Marker longitude={lng} latitude={lat} anchor='bottom'>
+          <Image src={pin} alt='location' width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
 };
 
 export default PropertyMap;
