69190b552d226e77ff5c5fc12b42164e1e959a16
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2026-02-19 00:44:33 -0800

    Add logic for pagination in properties page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,20 @@
 import PropertyCard from '@/components/PropertyCard';
 import connectDB from '@/config/database';
 import Property from '@/models/Property';
+import { clearStorage } from 'mapbox-gl';
 
-const PropertiesPage = async () => {
+const PropertiesPage = async ({ searchParams }) => {
+  const { page = 1, pageSize = 2 } = await searchParams;
+  // const { page = '1', pageSize = '2' } = await searchParams;
+  // const pageNumber = Number(page);
+  // console.log(page);
+  // console.log(pageSize);
+
   await connectDB();
-  const properties = await Property.find({}).lean();
+  const skip = (page - 1) * pageSize;
+
+  const total = await Property.countDocuments({});
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
 
   return (
     <section className='px-4 py-6'>
