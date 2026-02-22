993c90e762faa49c9c5025282c9f3eaa41da44f9
ahung1709 <115271537+ahung1709@users.noreply.github.com>
2026-02-22 14:49:46 -0800

    Update middleware to export a function

--- middleware.js
+++ middleware.js
@@ -1,4 +1,8 @@
-export { default } from 'next-auth/middleware';
+import nextAuthMiddleware from 'next-auth/middleware';
+
+export default function middleware(req) {
+  return nextAuthMiddleware(req);
+}
 
 export const config = {
   matcher: ['/properties/add', '/profile', '/properties/saved', '/messages'],
